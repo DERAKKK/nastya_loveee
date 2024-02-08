b77cb5454dac4c774511f3a0b6d49235815d815a
DERAKKK <121011427+DERAKKK@users.noreply.github.com>
2024-02-08 14:13:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+document.querySelector('.love__button').addEventListener("click", () => {
+    
+    document.querySelector('.love2').classList.toggle('fire')
+
+    document.querySelector('.love').classList.toggle('fire')
+
+})
+
+document.querySelector('.love2__button').addEventListener("click", () => {
+    
+    document.querySelector('.love3').classList.toggle('fire')
+
+    document.querySelector('.love2').classList.toggle('fire')
+
+})
+
+document.querySelector('.love3__button').addEventListener("click", () => {
+    
+    document.querySelector('.love4').classList.toggle('fire')
+
+    document.querySelector('.love3').classList.toggle('fire')
+
+})
